a3d6856b4b8c12963c88fb3a3ef5b6654f515a68
atmccann <allisontmccann@gmail.com>
2017-09-20 16:51:23 -0400

    scatterplot working!

--- src/_js/app.js
+++ src/_js/app.js
@@ -10,16 +10,19 @@ var _ = require('underscore');
 var d3 = require('d3');
 
 //data and chart
-var data = require("../_data/artists.json");
 var scatterChart = require('./chart.js');
+var data = require("../_data/artists.json");
+
 
 var scatterCharts = [],
     chart;
 
 function drawScatterCharts() {
+
     var chart = new scatterChart();
     chart.init("#scatter-container", data);
     scatterCharts.push(chart);
+
 }
 
 drawScatterCharts();
--- src/_js/chart.js
+++ src/_js/chart.js
@@ -17,7 +17,54 @@
 
         this.setupGraph = function(data) {
 
-            console.log(data[29]);
+
+            var array = _.map(data, function(d) {
+                return d;
+            });
+
+            var new_data = _.flatten(array);
+
+            console.log(new_data);
+
+            var margin = {top: 5, right: 5, bottom: 25, left: 25};
+
+            var width = 850,
+                height = 850;
+
+            var x = d3.scaleLinear().domain([1950,2017]).range([0, width]),
+                y = d3.scaleLinear().domain([0,100]).range([height, 0]);
+
+            var xAxis = d3.axisBottom(x)
+                yAxis = d3.axisLeft(y);
+
+            var svg = self.div.append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom);
+
+            var focus = svg.append("g")
+                .attr("class", "focus")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            var dots = focus.append("g");
+                dots.attr("clip-path", "url(#clip)");
+                dots.selectAll("dot")
+                    .data(new_data)
+                    .enter().append("circle")
+                    .attr('class', function(d) { return 'dot ' + d.artist; })
+                    .attr("r",5)
+                    .style("opacity", .5)
+                    .attr("cx", function(d) { return x(d.peak_year); })
+                    .attr("cy", function(d) { return y(d.norm_streams); })
+
+            focus.append("g")
+                  .attr("class", "axis axis--x")
+                  .attr("transform", "translate(0," + height + ")")
+                  .call(xAxis);
+
+            focus.append("g")
+                  .attr("class", "axis axis--y")
+                  .call(yAxis);
+
         };
 
     }
